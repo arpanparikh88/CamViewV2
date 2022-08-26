b8b091225f0a477e67104564bedfb4a39e92648d
Rodrigue Junior Nelson Medina <itsd1@anperva.com>
2022-08-26 09:17:16 -0700

    Add background image

--- CamViewV2/Pages/LoginLGT.cshtml.cs
+++ CamViewV2/Pages/LoginLGT.cshtml.cs
@@ -1,13 +1,25 @@
+using CamViewV2.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CamViewV2.Pages
 {
-
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public Login Login { get; set; }
         public void OnGet()
         {
+        }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var username = Login.Username;
+            return RedirectToPage("Index");
         }
     }
 }
